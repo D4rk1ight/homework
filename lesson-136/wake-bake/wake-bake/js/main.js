5f2500636cacd21d0f2f35e89427bf6ab7087981
D4rk1ight <147877294+D4rk1ight@users.noreply.github.com>
2025-11-14 23:13:29 +0300

    Add files via upload

---
+++ lesson-136/wake-bake/wake-bake/js/main.js
@@ -0,0 +1,70 @@
+
+const burgerStart = (event) => {
+    const target = event.target;
+    
+    const burgerIcon = target.closest('.burger-icon');
+    const burgerNavlink = target.closest('.nav__link'); 
+    const windowWidht = document.documentElement.clientWidth;
+
+    if (!burgerIcon && !burgerNavlink || windowWidht > 900) {
+        return; 
+    }
+    else if (!document.body.classList.contains('body--opened-menu')) {
+        document.body.classList.add('body--opened-menu');
+        }
+    else   {
+        document.body.classList.remove('body--opened-menu');
+        }
+    
+}
+
+document.addEventListener('click', burgerStart);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const burgerMenu = document.querySelector('.burger-icon');
+// const nav = document.querySelectorAll('nav__item');
+
+
+// const openMenu = (target) => {
+//     document.body.classList.toggle('body--opened-menu');
+    
+//     if (target === nav) {
+//     nav.classList.remove('body--opened-menu');
+//     }
+// } 
+
+// burgerMenu.addEventListener('click', openMenu);
+
+
+// function delHint (link) {
+// link.forEach(link => {
+//     link.removeAttribute('title');
+// });
+// }
+
+// button.addEventListener('mouseover', delHint);
+
+
+// const playIt = event => {
+//     const target = event.target;
+//     if (target == video) {
+//     video.classList.toggle('video__on');
+//     } 
+// }
+// button.addEventListener('click', playIt);
