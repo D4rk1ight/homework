352bf2c45e0fae4fa91faf61931f584f2eb2d020
d4rk1ight <graz9079@gmail.com>
2026-07-28 18:48:25 +0300

    tabs with js

--- PULSE/PULSE/js/accardion-locations.js
+++ PULSE/PULSE/js/accardion-locations.js
@@ -1,6 +1,16 @@
 console.log("accordion location has been init");
 
+const accordion = document.querySelector(".accordion");
 const accordionItem = document.querySelectorAll(".accordion__item");
+
+
+document.addEventListener('click', (e)=> {
+  
+  target = e.target; 
+
+  console.log(target.classList)
+
+})
 
 accordionItem.forEach(accordionButton => {
   accordionButton.addEventListener("click", () => {
